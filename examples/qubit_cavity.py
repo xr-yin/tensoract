3e923857a3d1d64aebdec5275864623a020ea1c2
Xianrui Yin <xianrui.yin@tum.de>
2025-08-07 16:04:41 +0200

    feat: update qubit_cavity.py to enhance simulation outputs and plotting, including entropy and purity calculations

--- examples/qubit_cavity.py
+++ examples/qubit_cavity.py
@@ -4,12 +4,13 @@ import torch
 
 import os
 import sys
+import pickle
 
 tensoractpath = os.path.dirname(os.path.abspath(os.getcwd()))
 sys.path.append(os.path.join(tensoractpath, "tensoract"))
     
 from tensoract import LPTN, LindbladOneSite
-from qutip import (basis, Qobj, expect, fidelity, hilbert_dist, mesolve, qeye, create, destroy, sigmax, sigmay, sigmaz,tensor)
+from qutip import (basis, Qobj, expect, entropy_mutual, fidelity, hilbert_dist, mesolve, qeye, create, destroy, sigmax, sigmay, sigmaz,tensor)
 
 def sim_QTP(d:int, alpha_l:float, alpha_cc:float, omega_c:float, omega_s:float, gamma:float, axes):
     # initial state
@@ -51,38 +52,26 @@ def sim_QTP(d:int, alpha_l:float, alpha_cc:float, omega_c:float, omega_s:float, 
     # cavity-cavity interaction
     H += alpha_cc * (cm_list[0] * cp_list[1] + cp_list[0] * cm_list[1])
 
-    times = np.linspace(0, 20, 100)
+    times = np.linspace(0, 40, 161)
 
     # collapse operators
     c_ops = [sm_list[0], cm_list[0], cm_list[1], sm_list[1]]
     c_ops = [np.sqrt(gamma) * op for op in c_ops]
 
     # evolution
-    result = mesolve(H, psi0, times, c_ops, [])
+    result = mesolve(H, psi0, times, c_ops)
 
     # Expectation value
     exp = expect([sz_list[0], cp_list[0]*cm_list[0], cp_list[1]*cm_list[1], sz_list[1]], result.states)
     # transform sigma_z from [-1, 1] to [0, 1]
     exp[0] = (exp[0] + 1) / 2
     exp[3] = (exp[3] + 1) / 2
-    # purity
-    purity = np.array([(rho*rho).tr() for rho in result.states])
-    #entropy = np.array([rho.ptrace(list(range(N//2))).entropy() for rho in result.states])
-    print('qtp:', [a[0] for a in exp], purity[-1])
-    axes[1].plot(times, purity, color='tab:orange', linestyle='--', label=r"QTP $\mathcal{P}$")
-    """
-    # Plot the expecation value
-    axes[0].semilogy(times, exp[0], color='tab:green', linestyle='-', label=r"QTP S1")
-    axes[0].semilogy(times, exp[1], color='tab:blue', linestyle='-', label=r"QTP C1")
-    axes[0].semilogy(times, exp[2], color='tab:blue', linestyle='--', label=r"QTP C2")
-    axes[0].semilogy(times, exp[3], color='tab:green', linestyle='--', label=r"QTP S2")
-    axes[0].semilogy(times, np.sum(exp, axis=0), color='tab:purple', linestyle='-', label=r"QTP total")
     # Plot the purity
-    axes[1].plot(times, purity, color='tab:orange', linestyle='--', label=r"QTP $\mathcal{P}$")
+    axes[1,0].plot(times, [rho.purity() for rho in result.states], color='tab:orange', linestyle='--', label=r"QTP $\mathcal{P}$")
     # Plot the entropy
-    #axes[2].plot(times, entropy, color='tab:red', linestyle='--', label=r"QTP $S$")
-    """
-    return result.states, times
+    axes[1,1].plot(times, [entropy_mutual(rho, [0,1], [2,3]) for rho in result.states], color='tab:red', linestyle='--', label=r"QTP $S$")
+
+    return result.states, exp
 
 def sim_TN(d:int, alpha_l:float, alpha_cc:float, omega_c:float, omega_s:float, gamma:float, axes):
     # initial state
@@ -94,29 +83,27 @@ def sim_TN(d:int, alpha_l:float, alpha_cc:float, omega_c:float, omega_s:float, g
     C1[0,0,0,0] = C2[0,0,3,0] = 1
     psi0 = LPTN([S1, C1, C2, S2])
 
-    initial = [psi0.copy(),]
-
     model = JCM(d, alpha_l, alpha_cc, omega_c, omega_s, gamma)
  
     options = {'tol':1e-12, 'disent_step': 1, 'disent_sweep':4, 'store_states': True}
     e_ops = [(model.sz + model.cid) / 2, model.num, model.num, (model.sz + model.cid) / 2]
 
     lab = LindbladOneSite(psi0, model)
-    lab.run(100, 0.2, 30, 30, e_ops=e_ops, options=options)
+    lab.run(160, 0.25, 25, 8, e_ops=e_ops, options=options)
 
-    times = lab.times
-    axes[0].semilogy(times, lab.expects[:,0], color='tab:green', label="TN S1")
-    axes[0].semilogy(times, lab.expects[:,1], color='tab:blue', label="TN C1")
-    axes[0].semilogy(times, lab.expects[:,2], color='tab:blue', label="TN C2")
-    axes[0].semilogy(times, lab.expects[:,3], color='tab:green', label="TN S2")
-    axes[0].semilogy(times, lab.expects.sum(-1), color='tab:purple', label="total")
-    axes[0].semilogy(times, 3*torch.exp(-gamma*torch.tensor(times)), label='interpolation')
+    ts, exps = lab.times, lab.expects
+    axes[0,0].semilogy(ts, exps[:,0], color='tab:green', label="TN S1")
+    axes[0,0].semilogy(ts, exps[:,1], color='tab:blue', label="TN C1")
+    axes[0,0].semilogy(ts, exps[:,2], color='tab:blue', label="TN C2")
+    axes[0,0].semilogy(ts, exps[:,3], color='tab:green', label="TN S2")
+    axes[0,0].semilogy(ts, exps.sum(-1), color='tab:purple', label="total")
+    axes[0,0].semilogy(ts, 3*torch.exp(-gamma*torch.tensor(ts)), label='interpolation')
     # Plot the purity
-    axes[1].plot(times, lab.purity, color='tab:orange', label=r"TN $\mathcal{P}$")
+    axes[1,0].plot(ts, lab.purity, color='tab:orange', label=r"TN $\mathcal{P}$")
     # Plot the entropy
-    axes[2].plot(times, lab.entropy[:, 4//2-1], color='tab:red', label=r"TN $S$")
-
-    return initial + lab.states
+    axes[1,1].plot(ts, lab.entropy[:, 4//2-1], color='tab:red', label=r"TN $S$")
+    
+    return lab.states, lab.expects, lab.times
 
 class JCM():
 
@@ -177,36 +164,49 @@ def main():
 
     d = 4
 
-    f, axes = plt.subplots(1, 3, figsize=(16, 5))
+    f, axes = plt.subplots(2, 2, figsize=(9, 9), sharex=True)
+    f.suptitle(r"Jaynes-Cummings Model with QTP and TN")
 
-    TN_states = sim_TN(d, alpha_l, alpha_cc, omega_c, omega_s, gamma, axes)
+    TN_states, TN_exps, times = sim_TN(d, alpha_l, alpha_cc, omega_c, omega_s, gamma, axes)
 
-    QTP_states, times = sim_QTP(d, alpha_l, alpha_cc, omega_c, omega_s, gamma, axes)
+    QTP_states, QTP_exps = sim_QTP(d, alpha_l, alpha_cc, omega_c, omega_s, gamma, axes)
 
     #assert np.allclose(TN_model.H_full().numpy(), QTP_model.full()), "TN and QTP models do not match"
 
+    # Plot the expecation value
+    axes[0,1].plot(times, QTP_exps[0][1:]-TN_exps[:,0].numpy(), color='tab:green', linestyle='-', label=r"diff S1")
+    axes[0,1].plot(times, QTP_exps[1][1:]-TN_exps[:,1].numpy(), color='tab:blue', linestyle='-', label=r"diff C1")
+    axes[0,1].plot(times, QTP_exps[2][1:]-TN_exps[:,2].numpy(), color='tab:blue', linestyle='--', label=r"diff C2")
+    axes[0,1].plot(times, QTP_exps[3][1:]-TN_exps[:,3].numpy(), color='tab:green', linestyle='--', label=r"diff S2")
+
     # infidelity
-    infidelity = np.array([1 - fidelity(Qobj(a.to_density_matrix(), dims=b.dims), b) for a, b in zip(TN_states, QTP_states)])
-    hs_dist = np.array([hilbert_dist(Qobj(a.to_density_matrix(), dims=b.dims), b) for a, b in zip(TN_states, QTP_states)])
+    infidelity = np.array([1 - fidelity(Qobj(a.to_density_matrix(), dims=b.dims), b) for a, b in zip(TN_states, QTP_states[1:])])
+    hs_dist = np.array([hilbert_dist(Qobj(a.to_density_matrix(), dims=b.dims), b) for a, b in zip(TN_states, QTP_states[1:])])
 
     print('infidelity', np.max(infidelity), infidelity[-1])
     print('hs_dist', np.max(hs_dist), hs_dist[-1])
 
     # inset ax
-    inset_ax = axes[1].inset_axes([0.5, 0.5, 0.4, 0.4])  # [x, y, width, height]
+    inset_ax = axes[0,1].inset_axes([0.5, 0.5, 0.4, 0.4])  # [x, y, width, height]
     inset_ax.semilogy(times, infidelity, color='tab:orange', linestyle='--', label=r"$IF$")
     inset_ax.semilogy(times, hs_dist, color='tab:blue', linestyle='--', label=r"$HS$")
     inset_ax.legend()
     
-    axes[0].set_ylabel(r"$\langle N \rangle$")
-    axes[1].set_ylabel(r"$\mathcal{P}$")
-    axes[2].set_ylabel(r"$S$")
+    axes[0,0].set_ylabel(r"$\langle N \rangle$")
+    axes[0,1].set_ylabel(r"$\langle N \rangle$")
+    axes[1,0].set_ylabel(r"$\mathcal{P}$")
+    axes[1,1].set_ylabel(r"$S$")
 
-    axes[0].set_ylim(1e-2, d)
+    axes[0,0].set_ylim(1e-2, d)
     for ax in axes.flat:
         ax.set_xlabel("Time")
         ax.legend(loc="lower right")
-    plt.savefig("data/jc_model_r4.pdf")
+    
+    plt.tight_layout()
+    plt.savefig("data/jc_model.pdf")
+
+    with open("data/jc_model.pkl", "wb") as f:
+        pickle.dump({"TN_states": TN_states, "QTP_states": QTP_states}, f)
 
 if __name__ == "__main__":
     main()
